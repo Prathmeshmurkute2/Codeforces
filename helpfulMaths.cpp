130c5dd20ba4245cc0c0b13caaf3045a1dda5fc6
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2026-08-14 04:20:59 +0530

    handling math

---
+++ helpfulMaths.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string s;
+
+    cin>>s;
+
+    for(int i=0;i<s.size();i+=2){
+        for(int j=i+2;j<s.size();j+=2){
+            if(s[i]-'0' >= s[j]-'0'){
+                swap(s[i],s[j]);
+            }
+        }
+    }
+    cout<<s;
+
+}
