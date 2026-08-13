8ae716a746e3ea86e4de6b51a4049080ae05aab4
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2026-08-13 07:28:20 +0530

    added bear and big brother

---
+++ BearandBigBrother.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int l,b;
+
+    cin>>l>>b;
+    int year=0;
+    while(l<=b){
+        l=l*3;
+        b=b*2;
+
+        year++;
+    }
+
+    cout<<year;
+}
