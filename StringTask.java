7979bf28fe2ad0811e20d016b7959b6137be49ee
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2026-08-16 18:54:16 +0530

    add solution for StringTask problem

---
+++ StringTask.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StringTask {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        s = s.toLowerCase();
+
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (c != 'a' && c != 'e' && c != 'i' &&
+                c != 'o' && c != 'u' && c != 'y') {
+
+                ans += "." + c;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
