eeb1e868f9effe08b974d89cf7b791ccde3c4279
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2026-08-16 20:19:19 +0530

    refactor Hamburgers solution for clarity and consistency

--- Hamburgers.java
+++ Hamburgers.java
@@ -6,7 +6,7 @@ public class Hamburgers {
 
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.nextLine();
+        String recipe = sc.next();
 
         int nb = sc.nextInt();
         int ns = sc.nextInt();
@@ -16,42 +16,53 @@ public class Hamburgers {
         int ps = sc.nextInt();
         int pc = sc.nextInt();
 
-        long rubles = sc.nextLong();   // IMPORTANT
+        long r = sc.nextLong();
 
+        // Count ingredients required for ONE hamburger
         int B = 0;
         int S = 0;
         int C = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < recipe.length(); i++) {
 
-            char c = s.charAt(i);
-
-            if (c == 'B')
+            if (recipe.charAt(i) == 'B') {
                 B++;
-            else if (c == 'S')
+            } else if (recipe.charAt(i) == 'S') {
                 S++;
-            else
+            } else {
                 C++;
+            }
         }
 
         long low = 0;
-        long high = 1_000_000_000_000L;
+        long high = 1_000_000_000_000L + 100;
 
         while (low <= high) {
 
             long mid = low + (high - low) / 2;
 
-            long needB = Math.max(0L, B * mid - nb);
-            long needS = Math.max(0L, S * mid - ns);
-            long needC = Math.max(0L, C * mid - nc);
+            // Ingredients needed for mid hamburgers
+            long requiredB = B * mid;
+            long requiredS = S * mid;
+            long requiredC = C * mid;
 
-            long cost = needB * pb
-                      + needS * ps
-                      + needC * pc;
+            // Ingredients we have to buy
+            long buyB = Math.max(0L, requiredB - nb);
+            long buyS = Math.max(0L, requiredS - ns);
+            long buyC = Math.max(0L, requiredC - nc);
 
-            if (cost <= rubles) {
+            // Total money required
+            long cost = buyB * pb
+                      + buyS * ps
+                      + buyC * pc;
+
+            if (cost <= r) {
+                // We can make mid hamburgers.
+                // Try making more.
                 low = mid + 1;
             } else {
+                // Too expensive.
+                // Try making fewer.
                 high = mid - 1;
             }
         }
